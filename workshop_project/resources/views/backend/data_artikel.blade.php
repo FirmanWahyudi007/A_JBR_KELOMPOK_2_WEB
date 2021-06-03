b7ca83c780ba08b456ebb6514a7d4deae0b3057a
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-06-03 15:21:18 +0700

    Create data_artikel.blade.php

---
+++ workshop_project/resources/views/backend/data_artikel.blade.php
@@ -0,0 +1,48 @@
+@extends('backend/layouts.template')
+@section('content')
+<div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
+    <div class="mdc-card p-0">
+        <h6 class="card-title card-padding pb-0 border-bottom">Data Donasi</h6>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th class="text-left">#</th>
+                        <th>Url</th>
+                        <th>Sampul</th>
+                        <th>Judul</th>
+                        <th>Isi Artikel</th>
+                        <th>Tanggal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-left">1</td>
+                        <td>www.gombel.com</td>
+                        <td>Donasi untuk Yayasan Nanda Delisha</td>
+                        <td>Contoh 1</td>
+                        <td>adasdwdasdw</td>
+                        <td>03/06/2021</td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">2</td>
+                        <td>www.gombel.com</td>
+                        <td>Donasi untuk Yayasan Nanda Delisha</td>
+                        <td>Contoh 1</td>
+                        <td>adasdwdasdw</td>
+                        <td>03/06/2021</td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">2</td>
+                        <td>www.gombel.com</td>
+                        <td>Donasi untuk Yayasan Nanda Delisha</td>
+                        <td>Contoh 1</td>
+                        <td>adasdwdasdw</td>
+                        <td>03/06/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
