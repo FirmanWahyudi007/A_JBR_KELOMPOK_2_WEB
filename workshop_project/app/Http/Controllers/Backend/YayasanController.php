34c6d7692338a65dd890779f9e787a90e76f590c
wahyudianpratiwi1 <71805820+wahyudianpratiwi1@users.noreply.github.com>
2021-06-03 14:59:24 +0700

    Yayasan

---
+++ workshop_project/app/Http/Controllers/Backend/YayasanController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class YayasanController extends Controller
+{
+    public function index(){
+        $yayasan = DB::table('yayasan')->get();
+        return view('backend.data_yayasan', compact('yayasan'));
+    }
+    public function create(){
+        $yayasan=null;
+        return view('backend.tambah_yayasan', compact('yayasan'));
+    }
+    public function store(Request $request){
+        DB::table('yayasan')->insert([
+            'nama_yayasan'=>$request->yayasan,
+            'alamat'=>$request->alamat,
+            'no_telp'=>$request->notelp,
+            'dokumentasi'=>'es.png'
+        ]);
+        return redirect()->route('yayasan.index')->with('success','Data Yayasan Telah Tersimpan');
+    }
+    public function edit($id){
+        $yayasan=DB::table('yayasan')->where('id',$id)->first();
+        return view('backend.tambah_yayasan',compact('yayasan'));
+    }
+    public function update(Request $request){
+         DB::table('yayasan')->where('id',$request->id)->update([
+                'nama_yayasan'=>$request->nama_yayasan,
+                'alamat'=>$request->alamat,
+                'no_telp'=>$request->no_telp,
+                'dokumentasi'=>$request->dokumentasi
+            ]);
+            return redirect()->route('yayasan.index')->with('success','Data Yayasan Telah Diperbaharui');
+            }
+    public function destroy($id){
+        DB::table('yayasan')->where('id',$id)->delete();
+        return redirect()->route('yayasan.index')->with('success','Data Yayasan Telah Dihapus');
+    }
+}
