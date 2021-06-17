1cf3a4e2446d26c4f3947b14039ad2ec30bff4b4
Firman Wahyudi <e41190305@student.polije.ac.id>
2021-06-17 08:04:46 +0700

    upp

--- workshop_project/app/Http/Controllers/Frontend/UserController.php
+++ workshop_project/app/Http/Controllers/Frontend/UserController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Artikel;
+use App\Models\Donasi;
 
 class UserController extends Controller
 {
     public function index()
     {
         # code...
-        return view('frontend.home');
+        $donasi = Donasi::all();
+        $artikel = Artikel::all();
+        return view('frontend.home', compact('artikel','donasi'));
     }
 }
